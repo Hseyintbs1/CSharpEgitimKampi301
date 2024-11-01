987778e6a86f3333b1f560dc5c4e039acdc989c0
Hüseyin TOPBAŞ <huseyintopba@gmail.com>
2024-11-01 23:07:27 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.EntityLayer/Concreate/Category.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concreate
+{
+    public class Category
+    {
+        /*
+        int x; // field
+        public int y { get; set; } // property
+
+        public void Test()
+        {
+            int z; // variable
+        }
+        */
+
+
+        public int CategoryId { get; set; } //Otomatik artan olduğunu anlaması için Class'ın isminin aynısı+Id kuralına uyulması gerekiyor.
+        public string CategoryName { get; set; }
+        public int CategoryStatus { get; set; }
+    }
+}
+/*
+ Field- variable- property
+
+
+int x; // field
+public int X { get; set; } // property
+
+ 
+ */
---
+++ CSharpEgitimKampi301.EntityLayer/Concreate/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concreate
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerSurname { get; set; }
+    }
+}
+
+/*
+        SOLİD
+        S: Single Responsibility Principle (Tek Sorumluluk ilkesi): Bir sınıfın bir tek sorumluluğu olmalıdır. Sadece bir iş yapmalıdır.
+        O: Open Closed Principle
+        L: Liskov Substitution Principle
+        I: Interface Segregation Principle
+        D: Dependency Inversion Principle
+ */
