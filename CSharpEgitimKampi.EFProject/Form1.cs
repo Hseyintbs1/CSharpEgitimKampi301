d9932ecfdac32d2e9c3c0b9e61771b8f05900faf
Hüseyin TOPBAŞ <huseyintopba@gmail.com>
2024-11-17 01:29:37 +0300

    ...

--- CSharpEgitimKampi.EFProject/Form1.cs
+++ CSharpEgitimKampi.EFProject/Form1.cs
@@ -16,5 +16,48 @@ namespace CSharpEgitimKampi.EFProject
         {
             InitializeComponent();
         }
+
+        EgitimKampiEFTravelDbEntities db = new EgitimKampiEFTravelDbEntities();
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var values = db.Guide.ToList();
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Guide guide = new Guide();
+            guide.GuideName = txtName.Text;
+            guide.GuideSurname = txtSurname.Text;
+            db.Guide.Add(guide);
+            db.SaveChanges();
+            MessageBox.Show("Guide Added");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var removeValue = db.Guide.Find(id);
+            db.Guide.Remove(removeValue);
+            db.SaveChanges();
+            MessageBox.Show("Guide Deleted");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var updateValue = db.Guide.Find(id);
+            updateValue.GuideName = txtName.Text;
+            updateValue.GuideSurname = txtSurname.Text;
+            db.SaveChanges();
+            MessageBox.Show("Guide Updated","uyarı",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+        }
+
+        private void btnGetByID_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var values = db.Guide.Where(x=>x.GuideId == id).ToList();
+            dataGridView1.DataSource = values;
+        }
     }
 }
--- CSharpEgitimKampi.EFProject/Guide.cs
+++ CSharpEgitimKampi.EFProject/Guide.cs
@@ -22,7 +22,7 @@ namespace CSharpEgitimKampi.EFProject
     
         public int GuideId { get; set; }
         public string GuideName { get; set; }
-        public string Guide1 { get; set; }
+        public string GuideSurname { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Location> Location { get; set; }
